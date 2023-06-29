5b6c1d7dd8be6d90d42151c178aa31ddf2b0543c
Ivan Sokol <sokolivan34@mail.ru>
2023-06-29 13:58:02 +0300

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TableAlamofire' do
+  pod 'Alamofire', '~> 5.5'
+
+end
---
+++ TableAlamofire/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TableAlamofire
+//
+//  Created by Ваня Сокол on 29.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
